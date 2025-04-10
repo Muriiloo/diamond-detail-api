16afd6ee817b49eea96ace4cd60e4efa86889d55
Muriiloo <murilooliveira110305@gmail.com>
2025-04-09 23:49:19 -0300

    feat: add get method in ServiceController to list services

--- src/main/java/com/diamonddetail/api/controller/ServiceController.java
+++ src/main/java/com/diamonddetail/api/controller/ServiceController.java
@@ -1,13 +1,14 @@
 package com.diamonddetail.api.controller;
 
 import com.diamonddetail.api.entities.ServiceEntity;
+import com.diamonddetail.api.record.services.ServiceResponseDTO;
 import com.diamonddetail.api.repository.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/services")
@@ -18,19 +19,45 @@ public class ServiceController {
 
     @PostMapping("/create-service")
     public ResponseEntity<?> createService(@RequestBody ServiceEntity service){
-        if(service.getName() == null || service.getName().isBlank()){
-            return ResponseEntity.badRequest().body("Digite o nome do service!");
-        }
+        try{
+            if(service.getName() == null || service.getName().isBlank()){
+                return ResponseEntity.badRequest().body("Digite o nome do service!");
+            }
 
-        if(service.getName().length() < 3){
-            return ResponseEntity.badRequest().body("O nome do serviço precisa ter mais de 3 caracteres!");
-        }
+            if(service.getName().length() < 3){
+                return ResponseEntity.badRequest().body("O nome do serviço precisa ter mais de 3 caracteres!");
+            }
 
-        if(!service.getName().matches("^[a-zA-ZÀ-ÿ\\s]+$")){
-            return ResponseEntity.badRequest().body("Nome do serviço não pode ter caracteres especiais!");
-        }
+            if(!service.getName().matches("^[a-zA-ZÀ-ÿ\\s]+$")){
+                return ResponseEntity.badRequest().body("Nome do serviço não pode ter caracteres especiais!");
+            }
 
-        return ResponseEntity.ok(serviceRepository.save(service));
+            return ResponseEntity.ok(serviceRepository.save(service));
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao criar serviço!");
+        }
+    }
+
+    @GetMapping("/list-service")
+    public ResponseEntity<?> listService(){
+        try{
+            List<ServiceEntity> services = serviceRepository.findAll();
+
+            if(services.isEmpty()){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum serviço encontrado!");
+            }
+
+            List<ServiceResponseDTO> response = services.stream().map(service -> new ServiceResponseDTO(
+                    service.getId(),
+                    service.getName(),
+                    service.getPrice()
+            )).toList();
+
+            return ResponseEntity.ok(response);
+
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao listar serviços!");
+        }
     }
 
 }

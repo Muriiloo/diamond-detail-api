de54b381172d291b5948b7a408da8e959b1bb61b
Muriiloo <murilooliveira110305@gmail.com>
2025-04-12 15:52:53 -0300

    fix: change UUID to Intenger in ServiceRepository

--- src/main/java/com/diamonddetail/api/repository/ServiceRepository.java
+++ src/main/java/com/diamonddetail/api/repository/ServiceRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ServiceRepository extends JpaRepository<ServiceEntity, UUID> {
+public interface ServiceRepository extends JpaRepository<ServiceEntity, Integer> {
 }

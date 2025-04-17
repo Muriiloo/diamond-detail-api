d7375d303784750861a4f00c3fc02b2baa5a45a2
Muriiloo <murilooliveira110305@gmail.com>
2025-04-17 00:12:20 -0300

    fix: adjust ID generation to auto-increment

--- src/main/java/com/diamonddetail/api/entities/UserEntity.java
+++ src/main/java/com/diamonddetail/api/entities/UserEntity.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 public class UserEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;

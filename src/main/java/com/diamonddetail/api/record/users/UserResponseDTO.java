245595d49aba1d32747e1df8acf82311f39ce061
Muriiloo <murilooliveira110305@gmail.com>
2025-04-17 00:11:39 -0300

    feat: add id field to user list response

--- src/main/java/com/diamonddetail/api/record/users/UserResponseDTO.java
+++ src/main/java/com/diamonddetail/api/record/users/UserResponseDTO.java
@@ -3,6 +3,6 @@ package com.diamonddetail.api.record.users;
 import com.diamonddetail.api.enums.UserType;
 
 
-public record UserResponseDTO(String name, String email, UserType type) {
+public record UserResponseDTO(Integer id, String name, String email, UserType type) {
 
 }
